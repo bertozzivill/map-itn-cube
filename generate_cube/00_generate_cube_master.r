ddcaac54f6cf7d4a3ac0c0626480c385d994a8bf
Amelia Bertozzi-Villa <bertozzivill@gmail.com>
2019-07-31 16:39:37 +0100

    update master script to accomodate new covariate prep code

--- generate_cube/00_generate_cube_master.r
+++ generate_cube/00_generate_cube_master.r
@@ -27,7 +27,7 @@ package_load(c("zoo","raster","VGAM", "doParallel", "data.table", "lubridate", "
                "rgdal", "INLA", "RColorBrewer", "cvTools", "boot", "stringr", "dismo", "gbm", "pryr"))
 
 # current dsub:
-# dsub --provider google-v2 --project map-special-0001 --image gcr.io/map-demo-0001/map_geospatial --regions europe-west1 --label "type=itn_cube" --machine-type n1-ultramem-40 --logging gs://map_users/amelia/itn/itn_cube/logs --input-recursive input_dir=gs://map_users/amelia/itn/itn_cube/input_data cov_dir=gs://mastergrids_5km func_dir=gs://map_users/amelia/itn/code/generate_cube/ --input CODE=gs://map_users/amelia/itn/code/generate_cube/00_generate_cube_master.r --output-recursive main_dir=gs://map_users/amelia/itn/itn_cube/results/20190729_new_covariates/ --command 'Rscript ${CODE}'
+# dsub --provider google-v2 --project map-special-0001 --image gcr.io/map-demo-0001/map_geospatial --regions europe-west1 --label "type=itn_cube" --machine-type n1-highmem-64 --disk-size 400 --logging gs://map_users/amelia/itn/itn_cube/logs --input-recursive input_dir=gs://map_users/amelia/itn/itn_cube/input_data cov_dir=gs://map_users/amelia/itn/itn_cube/results/covariates/20190729 func_dir=gs://map_users/amelia/itn/code/generate_cube/ --input CODE=gs://map_users/amelia/itn/code/generate_cube/00_generate_cube_master.r --output-recursive main_dir=gs://map_users/amelia/itn/itn_cube/results/20190729_new_covariates/ --command 'Rscript ${CODE}'
 
 ##  Environment Prep  ------------------------------------------------------------
 input_dir <- Sys.getenv("input_dir")
@@ -60,7 +60,7 @@ time_passed(tic, toc)
 print("STEP 3: Preparing covariates")
 tic <- Sys.time()
 source(file.path(func_dir, "03_prep_covariates.r"))
-prep_covariates(input_dir, cov_dir, func_dir, main_indir=main_dir, main_outdir=main_dir, prediction_years=start_year:end_year)
+prep_covariates(cov_dir,  main_indir=main_dir, main_outdir=main_dir)
 toc <- Sys.time()
 time_passed(tic, toc)
 
