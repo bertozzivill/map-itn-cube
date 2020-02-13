4707d667e396747ab3c96f46a5f42dba8338c98a
Amelia Bertozzi-Villa <bertozzivill@gmail.com>
2020-02-12 16:16:38 -0800

    update and beauitful relative gain plotting code

--- generate_cube/05_relative_gain.r
+++ generate_cube/05_relative_gain.r
@@ -26,7 +26,7 @@ in_dir <- "/Volumes/GoogleDrive/My Drive/itn_cube/results/20200204_no_ar1_effect
 # compare_dir <- file.path(zed_root, 'cubes/5km/ITN/')
 # out_dir <- "/Users/bertozzivill/Dropbox (IDM)/Malaria Team Folder/projects/map_itn_cube"
 
-colors <- c("#00a08a", "#d71b5a", "#f2a200", "#902e57", "#f98400","#5392c2")
+colors <- c("#00a08a", "#d71b5a", "#f2a200", "#902e57", "#5392c2", "#f98400")
 
 if (!dir.exists(in_dir)){
   stop("input directory not found! Did you remember to mount your drives?")
@@ -46,57 +46,79 @@ years <- 2000:2018
 
 
 # pdf(file.path(out_dir, "access_vs_use.pdf"), width=7, height=10)
-for (year in years){
-  print(year)
-  basename <- paste0("ITN_", year)
+# for (year in years){
+  # print(year)
+  # basename <- paste0("ITN_", year)
   
-  access <- raster(file.path(in_dir, paste0(basename, "_access.tif")))
-  use <- raster(file.path(in_dir, paste0(basename, "_use.tif")))
-  use_gap <- raster(file.path(in_dir, paste0(basename, "_use_gap.tif")))
+  access <- stack(file.path(in_dir, paste0("ITN_", years, "_access.tif")))
+  use <- stack(file.path(in_dir, paste0("ITN_", years, "_use.tif")))
+  use_gap <- stack(file.path(in_dir, paste0("ITN_", years, "_use_gap.tif")))
   access_gap <- 0.8-access
   
   
   # my definitions of access gain and use gain:
   
   # cap the use:access ratio at 1, to avoid large numbers
-  capped_use <- copy(use)
-  capped_use <- min(capped_use, access) 
-  
-  
+  capped_use <- stack(lapply(1:nlayers(use), function(idx){
+    min(use[[idx]], access[[idx]])
+  }))
+
   use_perc <- capped_use*100
   access_perc <- access*100
   
   # empty raster to fill with categorical values
   categorical <- copy(access)
-  categorical[categorical>0] <- 0
-  access_thresh <- 80
+  categorical <- calc(categorical, fun=function(x){ x[x > 0] <- 0; return(x)} )
+  # categorical[categorical>0] <- 0
+  access_thresh_1 <- 40
+  access_thresh_2 <- 80
+  use_thresh <- 0.8
   use_rate <- use_perc/access_perc
   
-  # class 1: Access >=80%, use rate >=1
-  categorical[access_perc>=access_thresh & use_rate>=1] <- 1
+  cat_labels <- data.table(ID=0:6,
+                           type=c("Not Modeled",
+                                  paste0("Access <", access_thresh_1, "%, Use Rate >=", use_thresh), 
+                                  paste0("Access <", access_thresh_1, "%, Use Rate <", use_thresh),
+                                  paste0("Access >=", access_thresh_1, "% & <", access_thresh_2, "%, Use Rate >=", use_thresh), 
+                                  paste0("Access >=", access_thresh_1, "% & <", access_thresh_2, "%, Use Rate <", use_thresh), 
+                                  paste0("Access >=", access_thresh_2, "%, Use Rate >=", use_thresh),
+                                  paste0("Access >=", access_thresh_2, "%, Use Rate <", use_thresh)
+                          ))
   
-  # class 2: Access >=80%, use rate <1
-  categorical[access_perc>=access_thresh & use_rate<1] <- 2
+  categorical <- stack(lapply(1:nlayers(categorical), function(idx){
+    print(idx)
+    this_layer <- categorical[[idx]]
+    
+    # class 1: High access and Use
+    this_layer[access_perc[[idx]] < access_thresh_1  & use_rate[[idx]] >= use_thresh] <- 1
+    
+    # class 1: High access and Use
+    this_layer[access_perc[[idx]] < access_thresh_1  & use_rate[[idx]] < use_thresh] <- 2
+    
+    # class 1: High access and Use
+    this_layer[access_perc[[idx]] >= access_thresh_1 & access_perc[[idx]] < access_thresh_2 & use_rate[[idx]] >= use_thresh] <- 3
+    
+    # class 2: High access, low Use
+    this_layer[access_perc[[idx]] >= access_thresh_1 & access_perc[[idx]] < access_thresh_2 & use_rate[[idx]] < use_thresh] <- 4
+    
+    # class 3: Low access, high use
+    this_layer[access_perc[[idx]] >= access_thresh_2 & use_rate[[idx]] >= use_thresh] <- 5
+    
+    # class 4: low access and use
+    this_layer[access_perc[[idx]] >= access_thresh_2 & use_rate[[idx]] < use_thresh] <- 6
+    
+    this_layer <- ratify(this_layer)
+    levels(this_layer) <- cat_labels
+    
+    return(this_layer)
+    
+  }))
   
-  # class 3: Access < 80%, use rate >=1
-  categorical[access_perc<access_thresh & use_rate>=1] <- 3
-  
-  # class 4: Access < 80%, use rate <1
-  categorical[access_perc<access_thresh & use_rate<1] <- 4
   
   # plot
-  categorical <- ratify(categorical)
-  attrs <- levels(categorical)[[1]]
-  attrs$type <- c("Not Modeled",
-                  "High Access and Use", 
-                  "High Access, Low Use",
-                  "Low Access, High Use",
-                  "Low Access and Use")
-  levels(categorical) <- attrs
-  
   max_pixels <- 2e5
   levelplot(categorical, att="type",
-            col.regions=c("#d3d3d3", colors[1:4]),
+            col.regions=c("#d3d3d3", colors),
             xlab=NULL, ylab=NULL, scales=list(draw=F),
             main="", margin=F, maxpixels=max_pixels)
   
@@ -122,7 +144,7 @@ for (year in years){
   # access_gain <- access_diff-use_perc
   
   
-}
+# }
 # graphics.off()
 
 
