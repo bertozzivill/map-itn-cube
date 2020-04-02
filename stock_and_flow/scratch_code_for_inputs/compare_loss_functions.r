f78fbe38cfdaf4623d50cbf59191eb42e14c6db6
Amelia Bertozzi-Villa <bertozzivill@gmail.com>
2020-04-02 09:39:32 -0700

    bound ITN retention half life at 1 year on the lower end, based on conversation with Pete 4/1/2020

--- stock_and_flow/scratch_code_for_inputs/compare_loss_functions.r
+++ stock_and_flow/scratch_code_for_inputs/compare_loss_functions.r
@@ -19,27 +19,31 @@ sigmoid<-function(t,k,L){
   return(v)	
 }
 
-n_param_samples <- 200
-k <- seq(16, 24, length.out = n_param_samples)
-L <- seq(4, 30, length.out = n_param_samples)
+n_param_samples <- 20
+# k <- seq(16, 24, length.out = n_param_samples)
+L <- seq(4, 20.7, length.out = n_param_samples)
 k <- 20
 
 samples <- data.table(expand.grid(k, L))
 names(samples) <- c("k", "L")
 
 all_sigs <- rbindlist(lapply(1:nrow(samples), function(idx){
-  time_points=seq(0,10,.01)
+  time_points=seq(0,max(L),.01)
   this_sigmoid <- data.table(id=idx,
     time=time_points,
     k=samples$k[idx],
     L=samples$L[idx],
-    sig=sigmoid(time_points, samples$k[idx], samples$L[idx]))
+    sig=sigmoid(time_points, samples$k[idx], samples$L[idx])
+    )
   return(this_sigmoid)
 }))
 
+all_sigs[, half_life:= L * sqrt(1- k/(k-log(0.5)))]
+unique(all_sigs[, list(L, half_life)])
 
 ggplot(all_sigs, aes(x=time, y=sig))  +
   geom_line(aes(color=as.factor(L), group=as.factor(id))) +
+  geom_vline(aes(xintercept=half_life, color=as.factor(L))) + 
   theme(legend.position="none") +
   labs(title=paste("k from", min(k), "to", max(k)))
 
