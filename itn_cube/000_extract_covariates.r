c957c9e3a8f9292bfe452650e7dbfb62f3b370bd
Mauricio van den Berg <mauricio.vandenberg@telethonkids.org.au>
2021-03-18 09:48:08 +0800

    move code in 000_extract_covariates.r script into functions

--- itn_cube/000_extract_covariates.r
+++ itn_cube/000_extract_covariates.r
@@ -9,30 +9,12 @@
 ## NB: This script requires an immense amount of memory, only rerun it if you absolutely must.
 ##############################################################################################################
 
-# DON'T USE THIS-- FOR FULL DSUB SEE 000_MAKE_DSUB.R
-# dsub --provider google-v2 --project map-special-0001 --image gcr.io/map-demo-0001/map_geospatial --regions europe-west1 --label "type=itn_cube" --machine-type n1-ultramem-40 --logging gs://map_users/amelia/itn/itn_cube/logs --input-recursive input_dir=gs://map_users/amelia/itn/itn_cube/input_data main_indir=gs://map_users/amelia/itn/itn_cube/results/covariates/20190729/ cov_dir=gs://mastergrids_5km --input run_individually=gs://map_users/amelia/itn/code/generate_cube/run_individually.txt CODE=gs://map_users/amelia/itn/code/generate_cube/03_prep_covariates.r --output-recursive main_outdir=gs://map_users/amelia/itn/itn_cube/results/covariates/20190729/ --command 'Rscript ${CODE}'
-
-
 package_load <- function(package_list){
   # package installation/loading
   new_packages <- package_list[!(package_list %in% installed.packages()[,"Package"])]
   if(length(new_packages)) install.packages(new_packages)
   lapply(package_list, library, character.only=T)
 }
-
-package_load(c("zoo","raster", "doParallel", "data.table", "rgdal", "RColorBrewer", "cvTools", "boot", "stringr", "dismo", "gbm"))
-
-if(Sys.getenv("input_dir")=="") {
-  input_dir <- "/Volumes/GoogleDrive/My Drive/itn_cube/input_data/"
-  main_indir <- "/Volumes/GoogleDrive/My Drive/itn_cube/results/20190614_rearrange_scripts/"
-  main_outdir <- "/Volumes/GoogleDrive/My Drive/itn_cube/results/20190614_rearrange_scripts/"
-} else {
-  input_dir <- Sys.getenv("input_dir")
-  main_indir <- Sys.getenv("main_indir")
-  main_outdir <- Sys.getenv("main_outdir")
-}
-
-prediction_years <- 2000:2021
 
 # Functions ------------------------------------------------------------
 
@@ -63,129 +45,152 @@ extract_values <- function(raster_fname_list, extraction_indices, reference_rast
   return(extracted_covs)
 }
 
-# Load list of covariates  ------------------------------------------------------------
+extract_covariates <- function(input_dir, main_indir, main_outdir, prediction_years) {
+  # Load list of covariates  ------------------------------------------------------------
 
-# load covariates
-cov_dt <- fread(file.path(main_indir, "covariate_key.csv"))
-cov_dt[, used_sam:= as.logical(used_sam)]
+  # load covariates
+  cov_dt <- fread(file.path(main_indir, "covariate_key.csv"))
+  cov_dt[, used_sam:= as.logical(used_sam)]
 
-# todo: remove this column when switching to new covariates
-cov_dt <- cov_dt[used_sam==T]
+  # todo: remove this column when switching to new covariates
+  cov_dt <- cov_dt[used_sam==T]
 
-# attach on list of vm directories passed into script via dsub
-vm_dirs <- data.table(cov_name=names(Sys.getenv(cov_dt$cov_name)), vm_path=Sys.getenv(cov_dt$cov_name))
-cov_dt <- merge(cov_dt, vm_dirs, by="cov_name", all.x=T)
+  # attach on list of vm directories passed into script via dsub
+  vm_dirs <- data.table(cov_name=names(Sys.getenv(cov_dt$cov_name)), vm_path=Sys.getenv(cov_dt$cov_name))
+  cov_dt <- merge(cov_dt, vm_dirs, by="cov_name", all.x=T)
 
-write.csv(cov_dt, file.path(main_outdir, "covariate_key.csv"), row.names=F)
+  write.csv(cov_dt, file.path(main_outdir, "covariate_key.csv"), row.names=F)
 
-# find the "valid" cell values for which we want to predict in the itn prediction step
-reference_raster <- raster(file.path(input_dir, "general/african_cn5km_2013_no_disputes.tif"))
-raster_indices <- which_non_null(reference_raster)
+  # find the "valid" cell values for which we want to predict in the itn prediction step
+  reference_raster <- raster(file.path(input_dir, "general/african_cn5km_2013_no_disputes.tif"))
+  raster_indices <- which_non_null(reference_raster)
 
-### Static covariates  ----------------------------------------------------------------------------#######################  
+  ### Static covariates  ----------------------------------------------------------------------------#######################
 
-print("Extracting static covariates")
-static_cov_dt <- cov_dt[type=="static", list(fname=file.path(vm_path, fname))]
+  print("Extracting static covariates")
+  static_cov_dt <- cov_dt[type=="static", list(fname=file.path(vm_path, fname))]
 
-all_static <- extract_values(static_cov_dt$fname, raster_indices, reference_raster)
-write.csv(all_static, file.path(main_outdir, "static_covariates.csv"), row.names = F)
+  all_static <- extract_values(static_cov_dt$fname, raster_indices, reference_raster)
+  write.csv(all_static, file.path(main_outdir, "static_covariates.csv"), row.names = F)
 
-rm(all_static); gc()
-print("static covariates successfully extracted")
+  rm(all_static); gc()
+  print("static covariates successfully extracted")
 
-### Annual covariates  ----------------------------------------------------------------------------#######################  
+  ### Annual covariates  ----------------------------------------------------------------------------#######################
 
-print("Extracting annual covariates")
-annual_cov_dt <- cov_dt[type=="year"]
+  print("Extracting annual covariates")
+  annual_cov_dt <- cov_dt[type=="year"]
 
-print("Extracting whole-continent values")
-ncores <- detectCores()
-print(paste("--> Machine has", ncores, "cores available"))
-registerDoParallel(ncores-2)
+  print("Extracting whole-continent values")
+  ncores <- detectCores()
+  print(paste("--> Machine has", ncores, "cores available"))
+  registerDoParallel(ncores-2)
 
-all_annual <- foreach(this_year=prediction_years) %dopar%{
-  
-  print(this_year)
-  
-  these_fnames <- copy(annual_cov_dt)
-  these_fnames[, year_to_use:=pmin(end_year, this_year)] # cap year by covariate availability
-  these_fnames[, year_to_use:=pmax(year_to_use, start_year)] 
-  
-  # if (this_year%%5>0){
-  #   these_fnames[fpath %like% "Population", fname:=gsub("YEAR", "YEAR-Interp", fname)]
-  # }
-  
-  these_fnames[, new_fname:=str_replace(fname, "YEAR", as.character(year_to_use))]
-  these_fnames[, full_fname:= file.path(vm_path, new_fname)]
-  
-  subset <- extract_values(these_fnames$full_fname, raster_indices, reference_raster, names=these_fnames$cov_name)
-  subset[, year:=this_year]
-  setcolorder(subset, c("year", "cellnumber", these_fnames$cov_name))
-  
-  return(subset)
+  all_annual <- foreach(this_year=prediction_years) %dopar%{
+
+    print(this_year)
+
+    these_fnames <- copy(annual_cov_dt)
+    these_fnames[, year_to_use:=pmin(end_year, this_year)] # cap year by covariate availability
+    these_fnames[, year_to_use:=pmax(year_to_use, start_year)]
+
+    # if (this_year%%5>0){
+    #   these_fnames[fpath %like% "Population", fname:=gsub("YEAR", "YEAR-Interp", fname)]
+    # }
+
+    these_fnames[, new_fname:=str_replace(fname, "YEAR", as.character(year_to_use))]
+    these_fnames[, full_fname:= file.path(vm_path, new_fname)]
+
+    subset <- extract_values(these_fnames$full_fname, raster_indices, reference_raster, names=these_fnames$cov_name)
+    subset[, year:=this_year]
+    setcolorder(subset, c("year", "cellnumber", these_fnames$cov_name))
+
+    return(subset)
+  }
+
+  all_annual <- rbindlist(all_annual)
+
+  landcov_names <- names(all_annual)[names(all_annual) %like% "Landcover"]
+  for (this_name in landcov_names){
+    all_annual[[this_name]] <- all_annual[[this_name]]/100
+  }
+
+  write.csv(all_annual, file.path(main_outdir, "annual_covariates.csv"), row.names = F)
+
+  rm(all_annual); gc()
+  print("annual covariates successfully extracted")
+
+  ### Fully dynamic covariates: extract and apply by month and year  ----------------------------------------------------------------------------#######################
+
+  print("Extracting dynamic covariates")
+
+  dynamic_cov_dt <- cov_dt[type=="yearmon"]
+  all_yearmons <- data.table(expand.grid(1:12, prediction_years))
+  names(all_yearmons) <- c("month", "year")
+
+  registerDoParallel(ncores-2)
+
+  dynamic_outdir <- file.path(main_outdir, "dynamic_covariates")
+  if (!dir.exists(dynamic_outdir)){
+    dir.create(dynamic_outdir)
+  }
+
+  all_dynamic <- foreach(month_index=1:nrow(all_yearmons)) %dopar% {
+
+    print(all_yearmons[month_index])
+
+    this_month <- all_yearmons[month_index]$month
+    this_year <- all_yearmons[month_index]$year
+
+    these_fnames <- copy(dynamic_cov_dt)
+    these_fnames[, year_to_use:=pmin(end_year, this_year)] # cap year by covariate availability
+    these_fnames[, year_to_use:=pmax(year_to_use, start_year)]
+
+    # cap to make sure the specific month_year is available
+    these_fnames[end_year==year_to_use & end_month<this_month, year_to_use:=end_year-1]
+    these_fnames[start_year==year_to_use & start_month>this_month, year_to_use:=start_year+1]
+
+    these_fnames[, new_fname:=str_replace(fname, "YEAR", as.character(year_to_use))]
+    these_fnames[, new_fname:=str_replace(new_fname, "MONTH", str_pad(this_month, 2, pad="0"))]
+    these_fnames[, full_fname:=file.path(vm_path, new_fname)]
+
+    subset <- extract_values(these_fnames$full_fname, raster_indices, reference_raster, names=these_fnames$cov_name)
+    subset[, year:=this_year]
+    subset[, month:=this_month]
+    setcolorder(subset, c("year", "month", "cellnumber", these_fnames$cov_name))
+
+    return(subset)
+  }
+  all_dynamic <- rbindlist(all_dynamic)
+
+  # save dynamic covariates(by year)
+  print("saving dynamic covariates by year")
+  for (this_year in prediction_years){
+    print(this_year)
+    write.csv(all_dynamic[year==this_year], file.path(dynamic_outdir, paste0("dynamic_", this_year,".csv")), row.names=F)
+  }
+  rm(all_dynamic); gc()
+  print("dynamic covariates successfully extracted")
 }
 
-all_annual <- rbindlist(all_annual)
+main <- function() {
+  package_load(c("zoo","raster", "doParallel", "data.table", "rgdal", "RColorBrewer", "cvTools", "boot", "stringr", "dismo", "gbm"))
 
-landcov_names <- names(all_annual)[names(all_annual) %like% "Landcover"]
-for (this_name in landcov_names){
-  all_annual[[this_name]] <- all_annual[[this_name]]/100
+  if(Sys.getenv("input_dir")=="") {
+    input_dir <- "/Volumes/GoogleDrive/My Drive/itn_cube/input_data/"
+    main_indir <- "/Volumes/GoogleDrive/My Drive/itn_cube/results/20190614_rearrange_scripts/"
+    main_outdir <- "/Volumes/GoogleDrive/My Drive/itn_cube/results/20190614_rearrange_scripts/"
+  } else {
+    input_dir <- Sys.getenv("input_dir")
+    main_indir <- Sys.getenv("main_indir")
+    main_outdir <- Sys.getenv("main_outdir")
+  }
+
+  prediction_years <- 2000:2021
+  extract_covariates(input_dir, main_indir, main_outdir, prediction_years)
 }
 
-write.csv(all_annual, file.path(main_outdir, "annual_covariates.csv"), row.names = F)
+# DON'T USE THIS-- FOR FULL DSUB SEE 000_MAKE_DSUB.R
+# dsub --provider google-v2 --project map-special-0001 --image gcr.io/map-demo-0001/map_geospatial --regions europe-west1 --label "type=itn_cube" --machine-type n1-ultramem-40 --logging gs://map_users/amelia/itn/itn_cube/logs --input-recursive input_dir=gs://map_users/amelia/itn/itn_cube/input_data main_indir=gs://map_users/amelia/itn/itn_cube/results/covariates/20190729/ cov_dir=gs://mastergrids_5km --input run_individually=gs://map_users/amelia/itn/code/generate_cube/run_individually.txt CODE=gs://map_users/amelia/itn/code/generate_cube/03_prep_covariates.r --output-recursive main_outdir=gs://map_users/amelia/itn/itn_cube/results/covariates/20190729/ --command 'Rscript ${CODE}'
 
-rm(all_annual); gc()
-print("annual covariates successfully extracted")
-
-### Fully dynamic covariates: extract and apply by month and year  ----------------------------------------------------------------------------#######################  
-
-print("Extracting dynamic covariates")
-
-dynamic_cov_dt <- cov_dt[type=="yearmon"]
-all_yearmons <- data.table(expand.grid(1:12, prediction_years))
-names(all_yearmons) <- c("month", "year")
-
-registerDoParallel(ncores-2)
-
-dynamic_outdir <- file.path(main_outdir, "dynamic_covariates")
-if (!dir.exists(dynamic_outdir)){
-  dir.create(dynamic_outdir)
-}
-
-all_dynamic <- foreach(month_index=1:nrow(all_yearmons)) %dopar% {
-  
-  print(all_yearmons[month_index])
-  
-  this_month <- all_yearmons[month_index]$month
-  this_year <- all_yearmons[month_index]$year
-  
-  these_fnames <- copy(dynamic_cov_dt)
-  these_fnames[, year_to_use:=pmin(end_year, this_year)] # cap year by covariate availability
-  these_fnames[, year_to_use:=pmax(year_to_use, start_year)] 
-  
-  # cap to make sure the specific month_year is available
-  these_fnames[end_year==year_to_use & end_month<this_month, year_to_use:=end_year-1]
-  these_fnames[start_year==year_to_use & start_month>this_month, year_to_use:=start_year+1]
-  
-  these_fnames[, new_fname:=str_replace(fname, "YEAR", as.character(year_to_use))]
-  these_fnames[, new_fname:=str_replace(new_fname, "MONTH", str_pad(this_month, 2, pad="0"))]
-  these_fnames[, full_fname:=file.path(vm_path, new_fname)]
-  
-  subset <- extract_values(these_fnames$full_fname, raster_indices, reference_raster, names=these_fnames$cov_name)
-  subset[, year:=this_year]
-  subset[, month:=this_month]
-  setcolorder(subset, c("year", "month", "cellnumber", these_fnames$cov_name))
-  
-  return(subset)
-}
-all_dynamic <- rbindlist(all_dynamic)
-
-# save dynamic covariates(by year)
-print("saving dynamic covariates by year")
-for (this_year in prediction_years){
-  print(this_year)
-  write.csv(all_dynamic[year==this_year], file.path(dynamic_outdir, paste0("dynamic_", this_year,".csv")), row.names=F)
-}
-rm(all_dynamic); gc()
-print("dynamic covariates successfully extracted")
-
+main()
